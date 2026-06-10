11fa7affce85b4d36681855b945249bd26395ec6
ESHXNCODES <priyadarshieshan2@gmail.com>
2026-06-10 22:35:46 +0530

    Add find the index of the first occurence  in string Solution

---
+++ Week_2/Day_3/Find_the_index_of_the_firstoccurence_in_string.cpp
@@ -0,0 +1,10 @@
+        int m=haystack.length();
+        int n=needle.length();
+        int pointer=-1;
+        for(int i=0;i<m-n;i++){
+            if(haystack.substr(i,n)==needle){
+                pointer=i;
+                break;
+                
+            }
+        }
